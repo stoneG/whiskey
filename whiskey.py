78d30acbc4f3291eda7d0206a65ac62f4bcb6bd0
stoneG <me@sitong>
2012-12-02 18:56:06 -0500

    basic server app functionality defined, currently server prints to console instead of sending back to the client

--- whiskey.py
+++ whiskey.py
@@ -9,16 +9,18 @@ from urlparse import urlparse
 
 
 class Whiskey(object):
-    def __init__(self, app):
-        self.app = app
+    def __init__(self, addr):
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.headers_set = []
         self.headers_sent = []
+        self.addr = addr
+        self.brew(self.s)
 
-    def brew(self, socket, addr):
+    def brew(self, socket):
         """Make server method."""
 
-        self.s.bind(addr)
+        self.s.bind(self.addr)
         self.s.listen(3)
 
         env = self.environ = dict(os.environ.items())
@@ -30,58 +32,84 @@ class Whiskey(object):
         env['wsgi.run_once'] = True
         env['wsgi.url_scheme'] = 'http'
 
-    def drink(self):
+    def drink_forever(self):
         """Run method."""
         while True:
             print '\nWaiting for connection'
             conn, addr = self.s.accept()
             pid = os.fork()
             if pid == 0:
+                #pdb.set_trace()
                 print 'Accepted connection from:', addr
                 request = conn.recv(1024)
-                request = msg.decode()
+                request = request.decode()
                 parse = HTTPRequest(request)
                 bartender = Bartender()
-                self.environ = bartender.build_environ(parse)
+                self.environ = bartender.build_environ(self.environ, parse)
                 if self.environ == None:
                     conn.close()
                     os._exit(0)
                 else:
-                    # pass to app
-                    os._exit(0)
+                    result = self.app(self.environ, self.start_response)
+                    try:
+                        for data in result:
+                            if data:    # don't send headers until body appears
+                                self.respond(data)
+                        if not headers_sent:
+                            self.respond('')   # send headers now if body was empty
+                    finally:
+                        if hasattr(result, 'close'):
+                            result.close()
+                        conn.close()
+                        os._exit(0)
             else:
                 conn.close()
                 continue
 
-    def start_response(status, response_headers, exc_info=None):
-        if exc_info:
-            try:
-                if self.headers_sent:
-                    # Re-raise original exception if headers sent
-                    raise exc_info[0], exc_info[1], exc_info[2]
-            finally:
-                exc_info = None     # avoid dangling circular ref
-        elif self.headers_set:
-            raise AssertionError("Headers already set!")
+    #def start_response(status, response_headers, exc_info=None):
+    #    if exc_info:
+    #        try:
+    #            if self.headers_sent:
+    #                # Re-raise original exception if headers sent
+    #                raise exc_info[0], exc_info[1], exc_info[2]
+    #        finally:
+    #            exc_info = None     # avoid dangling circular ref
+    #    elif self.headers_set:
+    #        raise AssertionError("Headers already set!")
 
-        self.headers_set = [status, response_headers]
+    #    self.headers_set[:] = [status, response_headers]
 
-def add_to_environ(key, env, parse):
-    env[key] = parse.headers
-
-    def start_response(status, response_headers, exc_info=None):
+    def start_response(self, status, response_headers, exc_info=None):
         pass
+
+    #def respond(self, data):
+    #    if not self.headers_set:
+    #         raise AssertionError("write() before start_response()")
+
+    #    elif not self.headers_sent:
+    #         # Before the first output, send the stored headers
+    #         status, response_headers = self.headers_sent[:] = self.headers_set
+    #         print 'Status: %s\r\n' % status
+    #         for header in response_headers:
+    #             print '%s: %s\r\n' % header
+    #         print '\r\n'
+
+    def respond(self, data):
+        print data
+
+    def set_app(self, app):
+        self.app = app
+
 
 class Bartender(object):
     """Handles whiskey requests."""
 
-    def build_environ(self, parse):
+    def build_environ(self, env, parse):
         if parse.error_code != None:
             # Panic
             print 'Parse Error Code: {0}\nMessage: {1}'.format(parse.error_code,
                                                                parse.error_message)
             return None
-        env = self.environ
         # See PEP 333 for definitions
         h = parse.headers
         env['REQUEST_METHOD'] = parse.command
@@ -105,7 +133,7 @@ class Bartender(object):
                 env['HTTP_'+key] = val
 
         url = urlparse(parse.path)
-        if url.path == ''
+        if url.path == '':
             url.path = '/'
         path_split = url.path.split('/')
         env['SCRIPT_NAME'] = '/'.join(path_split[:-1])
@@ -136,6 +164,7 @@ class HTTPRequest(BaseHTTPRequestHandler):
 
 def distill(host, port, app, server=Whiskey, handler=Bartender):
     """Instantiate Whiskey on host and port, communicating with app."""
-    whiskey = Whiskey((host, port), handler)
+    whiskey = Whiskey((host, port))
     whiskey.set_app(app)
-    return server
+    whiskey.drink_forever()
+    return whiskey
