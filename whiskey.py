1667b3f0ad8c71a6aa4486de5cce1ad94099558d
stoneG <me@sitong>
2012-12-04 14:58:37 -0500

    improving readability and improving on function based on a code review from Dave

--- whiskey.py
+++ whiskey.py
@@ -10,30 +10,22 @@ from urlparse import urlparse
 
 class Whiskey(object):
     def __init__(self, addr):
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.headers_set = []
         self.headers_sent = []
         self.addr = addr
-        self.brew(self.s)
+        self.make_server()
 
-    def brew(self, socket):
-        """Make server method."""
+    def make_server(self):
+        """Instantiates socket to self.s."""
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    def drink_forever(self):
+        """Run method."""
 
         self.s.bind(self.addr)
         self.s.listen(3)
 
-        env = self.environ = dict(os.environ.items())
-        env['wsgi.input'] = sys.stdin
-        env['wsgi.errors'] = sys.stderr
-        env['wsgi.version'] = (1, 0)
-        env['wsgi.multithread'] = False
-        env['wsgi.multiprocess'] = True
-        env['wsgi.run_once'] = True
-        env['wsgi.url_scheme'] = 'http'
-
-    def drink_forever(self):
-        """Run method."""
         while True:
             print '\nWaiting for connection'
             conn, addr = self.s.accept()
@@ -42,32 +34,31 @@ class Whiskey(object):
                 print 'Accepted connection from:', addr
                 request = conn.recv(1024)
                 request = request.decode()
-                parse = HTTPRequest(request)
-                bartender = Bartender()
-                self.environ = bartender.build_environ(self.environ, parse)
-                if self.environ == None:
+                try:
+                    self.build_environ(request)
+                except ParseError:
+                    self.whiskey_response('400 Bad Request', conn)
                     conn.close()
                     os._exit(0)
                 else:
                     result = self.app(self.environ, self.start_response)
                     try:
-                        for body in result:
-                            if body:    # don't send headers until body appears
-                                self.respond(body, conn)
+                        for element in result:
+                            if element: # don't send headers until elements appear
+                                self.app_response(element, conn)
                         if not self.headers_sent:
-                            self.respond('', conn)   # send headers now if body was empty
+                            self.app_response('', conn) # send headers now if body was empty
                     finally:
                         if hasattr(result, 'close'):
-                            result.close()
+                            result.close() # in case result is a file object
                         print 'Closing connection'
                         conn.close()
                         os._exit(0)
             else:
                 conn.close()
-                continue
 
     def start_response(self, status, response_headers, exc_info=None):
-        """Sets response headers to self.headers_set"""
+        """Sets response headers to self.headers_set."""
         if exc_info:
             try:
                 if self.headers_sent:
@@ -80,47 +71,66 @@ class Whiskey(object):
 
         self.headers_set[:] = [status, response_headers]
 
-    def respond(self, body, conn):
-        """Responds to client."""
-        response = body
+    def app_response(self, element, conn, status=None):
+        """Sends app response to client."""
+        response = element
         if not self.headers_set:
-             raise AssertionError("respond() before start_response()")
+             raise AssertionError("Trying to respond() before start_response()")
 
         elif not self.headers_sent:
              # Before the first output, send the stored headers
              status, headers = self.headers_sent[:] = self.headers_set
              rn = '\r\n'
-             headers = [' : '.join(header) for header in headers]
-             response = status + rn + rn.join(headers) + rn*2 + body
+             headers = [': '.join(header) for header in headers]
+             response = 'HTTP/1.1' + status + rn + rn.join(headers) + rn*2 + body
 
+        response = response.encode('utf-8')
         print 'Serving request'
         conn.send(response)
+
+    def whiskey_response(self, status, conn):
+        """Sends whiskey response to client."""
+        if status:
+            status = status.encode('utf-8')
+            conn.send(status)
 
     def set_app(self, app):
         self.app = app
 
+    def build_environ(self, request):
+        """Given the request unicode str, build self.environ."""
 
-class Bartender(object):
-    """Handles whiskey requests."""
+        request = Parsed(request)
 
-    def build_environ(self, env, parse):
-        if parse.error_code != None:
-            # Panic
-            print 'Parse Error Code: {0}\nMessage: {1}'.format(parse.error_code,
-                                                               parse.error_message)
-            return None
         # See PEP 333 for definitions
-        h = parse.headers
-        env['REQUEST_METHOD'] = parse.command
+        # WSGI specific environ variables
+        env = self.environ = dict(os.environ.items())
+        env['wsgi.input'] = sys.stdin
+        env['wsgi.errors'] = sys.stderr
+        env['wsgi.version'] = (1, 0)
+        env['wsgi.multithread'] = False
+        env['wsgi.multiprocess'] = True
+        env['wsgi.run_once'] = True
+        env['wsgi.url_scheme'] = 'http'
+
+        if request.error_code != None:
+            err = 'Parse Error Code: {0}\nMessage: {1}'.format(request.error_code,
+                                                               request.error_message)
+            # TODO: Send some error code to the client
+            print 'TODO: send error code to client'
+            raise ParseError(err)
+
+        # Shared with CGI
+        h = request.headers
+        env['REQUEST_METHOD'] = request.command
         env['CONTENT_TYPE'] = h.type
         env['CONTENT_LENGTH'] = h['content-length'] if 'content-length' in h else ''
         env['SERVER_NAME'] = h['host'].split(':')[0] if 'host' in h else ''
         env['SERVER_PORT'] = h['host'].split(':')[1] if 'host' in h else ''
-        env['SERVER_PROTOCOL'] = parse.request_version
-        # REMOTE_HOST
-        # REMOTE_ADDR
+        env['SERVER_PROTOCOL'] = request.request_version
+        # NOTE: Maybe add optional environ variables ie: REMOTE_HOST, REMOTE_ADDR
 
-        for header in parse.headers.headers:
+        for header in request.headers.headers:
             key, val = header.split(':', 1)
             key = key.replace('-', '_').upper()
             val = val.strip()
@@ -131,20 +141,17 @@ class Bartender(object):
             else:
                 env['HTTP_'+key] = val
 
-        url = urlparse(parse.path)
+        url = urlparse(request.path)
         if url.path == '':
             url.path = '/'
         env['SCRIPT_NAME'] = ''
         env['PATH_INFO'] = url.path
-        #path_split = url.path.split('/')
-        #env['SCRIPT_NAME'] = '/'.join(path_split[:-1])
-        #path_info = path_split[-1] if path_split[-1] != '' else ''
-        #env['PATH_INFO'] = path_info
         env['QUERY_STRING'] = url.query
-        return env
 
+class Bartender(object):
+    """Handles whiskey requests."""
 
-class HTTPRequest(BaseHTTPRequestHandler):
+class Parse(BaseHTTPRequestHandler):
     """Parses HTTP Requests."""
     def __init__(self, request):
         self.rfile = StringIO(request)
@@ -155,6 +162,12 @@ class HTTPRequest(BaseHTTPRequestHandler):
     def send_error(self, code, message):
         self.error_code = code
         self.error_message = message
+
+class ParseError(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
 
 def distill(host, port, app, server=Whiskey, handler=Bartender):
     """Instantiate Whiskey on host and port, communicating with app."""
