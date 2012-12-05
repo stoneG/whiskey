57c9f461d67b74ffe0f1b383377eb8690e0e7bab
stoneG <me@sitong>
2012-12-05 15:04:05 -0500

    added date_time and other response header support

--- whiskey.py
+++ whiskey.py
@@ -1,4 +1,5 @@
 from BaseHTTPServer import BaseHTTPRequestHandler # used for request parsing
+from datetime import datetime
 import os
 import pdb
 from StringIO import StringIO
@@ -9,6 +10,29 @@ from urlparse import urlparse
 
 
 class Whiskey(object):
+    """Whiskey is a WSGI compliant server. It supports the following
+    environ variables:
+
+    CONTENT_LENGTH    :
+    CONTENT_TYPE      :
+    HTTP_             :
+    PATH_INFO         :
+    QUERY_STRING      :
+    REQUEST_METHOD    :
+    SCRIPT_NAME       :
+    SERVER_NAME       :
+    SERVER_PROTOCOL   :
+    SERVER_PORT       :
+    wsgi.errors       :
+    wsgi.input        :
+    wsgi.multiprocess :
+    wsgi.multithread  :
+    wsgi.run_once     :
+    wsgi.url_scheme   :
+    wsgi.version      :
+    """
+
+
     def __init__(self, addr):
         self.headers_set = []
         self.headers_sent = []
@@ -42,6 +66,7 @@ class Whiskey(object):
                     os._exit(0)
                 else:
                     result = self.app(self.environ, self.start_response)
+                    self.add_server_headers(result)
                     try:
                         for element in result:
                             if element: # don't send headers until elements appear
@@ -58,7 +83,19 @@ class Whiskey(object):
                 conn.close()
 
     def start_response(self, status, response_headers, exc_info=None):
-        """Sets response headers to self.headers_set."""
+        """start_response is passed to the app, which sets response headers
+        to self.headers_set.
+
+        The app may call start_response multiple times, if and only if it
+        provides exc_info.
+
+        start_response must also return a response callable that legacy apps can
+        use as a file-like stream. This makes it easy to convert old apps to be
+        "WSGI compliant". Its use is deprecated because using a file-like stream
+        puts the reins of execution in the hands of the app object, preventing
+        intelligent content handling interleaving/asynchrony on the part of the
+        server.
+        """
         if exc_info:
             try:
                 if self.headers_sent:
@@ -66,26 +103,36 @@ class Whiskey(object):
                     # which should abort the app
                     raise exc_info[0], exc_info[1], exc_info[2]
             finally:
-                exc_info = None     # avoid dangling circular ref TODO: what is this?
+                exc_info = None     # NOTE: this may not be necessary
         elif self.headers_set:
-            raise AssertionError("Headers already set!")
+            raise AssertionError("Multiple calls to start_response,"+
+                                 " headers already set!")
 
         self.headers_set = [status, response_headers]
-        self._additional_headers()
+        self.error_checking()
         return self.app_response
-    # NOTE: start_response must also return a write_fn callable that legacy apps can
-    # use as a file-like stream. This makes it easy to convert old apps to be
-    # "WSGI compliant". Its use is deprecated because using a file-like stream puts
-    # the reins of execution in the hands of the app object, preventing
-    # intelligent content handling interleaving/asynchrony on the part of the server.
 
-    def additional_headers(self):
+    def add_server_headers(self, response):
         """Adds additional headers that the app omits."""
         # TODO: figure out what needs to go here
-        # Date:
-        # Server:
-        pass
+        server_headers = ['Date', 'Server']
+        if self.headers_set:
+            headers_set_keys = [key for key, value in self.headers_set]
+            for header in server_headers:
+                for index, key in enumerate(headers_set_keys):
+                    if header == key:
+                        del self.headers_set[index]
+        for header in server_headers:
+            self.headers_set += self.determine(header)
 
+    def determine(header):
+        """Given a header key, return the tuple (header, value)."""
+        if header == 'Date':
+            return (header, self.date_time())
+
+    def error_checking(self):
+        # TODO: figure out what needs to go here
+        pass
 
     def app_response(self, element, conn, status=None):
         """Sends app response to client."""
@@ -101,13 +148,14 @@ class Whiskey(object):
              response = 'HTTP/1.1' + status + rn + rn.join(headers) + rn*2 + element
 
         response = response.encode('utf-8')
-        print 'Serving request'
+        print 'Serving app response'
         conn.send(response)
 
     def server_response(self, status, conn):
         """Sends server response to client."""
         if status:
             status = status.encode('utf-8')
+            print 'Serving server response'
             conn.send(status)
 
     def set_app(self, app):
@@ -162,6 +210,28 @@ class Whiskey(object):
         env['SCRIPT_NAME'] = ''
         env['PATH_INFO'] = url.path
         env['QUERY_STRING'] = url.query
+
+        # Optional
+        env['SERVER_SOFTWARE'] = 'Whiskey/0.1 ' +
+                                 'Python/{0}.{1}.{2}'.format(v[0], v[1], v[2])
+        env['GATEWAY_INTERFACE'] = 'CGI/1.1'
+        #wsgi.file_wrapper: wsgiref.util.FileWrapper
+        #REMOTE_ADDR: 127.0.0.1
+        #REMOTE_HOST: localhost
+
+    def date_time():
+        """Return a string representation of a date according to RFC 1123
+        (HTTP/1.1).
+
+        The supplied date must be in UTC.
+
+        """
+        dt = datetime.now()
+        weekday = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"][dt.weekday()]
+        month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep",
+                 "Oct", "Nov", "Dec"][dt.month - 1]
+        return "%s, %02d %s %04d %02d:%02d:%02d GMT" % (weekday, dt.day, month,
+            dt.year, dt.hour, dt.minute, dt.second)
 
 class Parse(BaseHTTPRequestHandler):
     """Parses HTTP Requests."""
